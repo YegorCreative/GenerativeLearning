93e21b217ff159b362cefd86e7514f64b903953c
YegorCreative <37757944+YegorCreative@users.noreply.github.com>
2020-07-22 14:05:58 -0500

    Add files via upload

---
+++ Javascript/scripts.js
@@ -0,0 +1,22 @@
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+/*  close button*/
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
